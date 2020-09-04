894f726f95af64a563175a9716a265ada3e8d2eb
Tristan <tehosken1@gmail.com>
2020-09-04 20:20:57 +0100

    load items and transactions to postgres

--- spark/load_db.py
+++ spark/load_db.py
@@ -1,20 +1,75 @@
+import logging
 import os
 import sys
+import time
 
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import split
 
 from database import csv, jdbc
 from utils import session_builder
 
 
-def transform(df: DataFrame) -> DataFrame:
+def transform_people(df: DataFrame) -> DataFrame:
     # Assumes only ever two names
     return (
-        df.withColumn("first_name", split("name", " ").getItem(0))
-        .withColumn("last_name", split("name", " ").getItem(1))
+        df.withColumn("FIRST_NAME", split("NAME", " ").getItem(0))
+        .withColumn("LAST_NAME", split("NAME", " ").getItem(1))
         .withColumn("AGE", df["AGE"].cast("INT"))
     )
+
+
+def transform_items(df: DataFrame) -> DataFrame:
+    return df.withColumn("PRICE", df["PRICE"].cast("FLOAT"))
+
+
+def transform_transactions(df: DataFrame) -> DataFrame:
+    return df
+
+
+def load_people(
+    spark_session: SparkSession,
+    data_dir: str,
+    postgres_url: str,
+    postgres_driver_class: str,
+):
+    people_df = csv.get_df_from_csv_directory(
+        spark_session, os.path.join(data_dir, "person")
+    )
+
+    people_df = transform_people(people_df)
+
+    jdbc.load(people_df, postgres_url, "people", postgres_driver_class)
+
+
+def load_items(
+    spark_session: SparkSession,
+    data_dir: str,
+    postgres_url: str,
+    postgres_driver_class: str,
+):
+    items_df = csv.get_df_from_csv_directory(
+        spark_session, os.path.join(data_dir, "item")
+    )
+
+    items_df = transform_items(items_df)
+
+    jdbc.load(items_df, postgres_url, "items", postgres_driver_class)
+
+
+def load_transactions(
+    spark_session: SparkSession,
+    data_dir: str,
+    postgres_url: str,
+    postgres_driver_class: str,
+):
+    transactions_df = csv.get_df_from_csv_directory(
+        spark_session, os.path.join(data_dir, "transaction")
+    )
+
+    transactions_df = transform_transactions(transactions_df)
+
+    jdbc.load(transactions_df, postgres_url, "transactions", postgres_driver_class)
 
 
 if __name__ == "__main__":
@@ -28,17 +83,12 @@ if __name__ == "__main__":
 
     data_directory = sys.argv[1]
 
+    logger = logging.getLogger(__name__)
+
     spark = session_builder(POSTGRS_DRIVER_JAR_PATH).getOrCreate()
-
-    people_df = csv.get_df_from_csv_directory(
-        spark, os.path.join(data_directory, "person")
-    )
-    people_df.show()
-
-    people_df = transform(people_df)
-    people_df.show()
-    print(people_df.schema)
-
-    jdbc.load(people_df, POSTGRES_URL, "people", POSTGRES_DRIVER_CLASS)
-
-    # TODO load items and transactions
+    t0 = time.time()
+    load_people(spark, data_directory, POSTGRES_URL, POSTGRES_DRIVER_CLASS)
+    load_items(spark, data_directory, POSTGRES_URL, POSTGRES_DRIVER_CLASS)
+    load_transactions(spark, data_directory, POSTGRES_URL, POSTGRES_DRIVER_CLASS)
+    load_time = time.time() - t0
+    logger.info(f"Loaded in {load_time} seconds")
